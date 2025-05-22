df2c897d70fc1fd9f713253ecfe5df24948563f2
Pombero <jltabbia@gmail.com>
2025-05-22 16:15:53 -0300

    Agregue una linea en blanco antes de declarara la clase

--- app/src/main/java/ar/com/jtsistemas/estadisticasnewcom/MainActivity.kt
+++ app/src/main/java/ar/com/jtsistemas/estadisticasnewcom/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.material3.Text
 import ar.com.jtsistemas.estadisticasnewcom.ui.theme.EstadisticasNewcomTheme
 
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
